503737fbb8f78e164b09639347b9b54425440434
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-12-05 12:12:38 +0500

    I have created our simple command line calculator.

--- app.ts
+++ app.ts
@@ -12,12 +12,11 @@ let sleep = () => {
 }
 
 async function welcome() {
-   let rainbowTitle =  chalkAnimation.rainbow("let's start calculation...");
-   await sleep()  
-   rainbowTitle.stop(); 
-   console.log(`
-    ${
-      chalk.cyanBright(
+    let rainbowTitle = chalkAnimation.rainbow("let's start calculation...");
+    await sleep()
+    rainbowTitle.stop();
+    console.log(`
+    ${chalk.cyanBright(
         `
         _____________________
         |  _________________  |
@@ -35,17 +34,82 @@ async function welcome() {
         |_____________________|
         
         `
-      )
-    }    
+    )
+        }    
     `
-   );
+    );
 
-   console.log(    
-    chalk.cyanBright(     
-       gradient.pastel.multiline(figlet.textSync("CALCULATOR", {horizontalLayout: 'full'}))         
-    )    
-   );
+    console.log(
+        chalk.cyanBright(
+            gradient.pastel.multiline(figlet.textSync("CALCULATOR", { horizontalLayout: 'full' }))
+        )
+    );
 
 
 }
+
 await welcome()
+
+async function askQuestion() {
+    let answer = await inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'operator',
+                message: 'Which operation you want to perfom? \n',
+                choices: ['addition', 'subtraction', 'multiplication', 'division'],
+            },
+            {
+                type: 'number',
+                name: 'num1',
+                message: 'enter number 1',
+                validate: (answer) => {
+                    if (isNaN(answer)) {
+                        return "Please enter a valid number";
+                    }
+                    return true;
+                }
+            },
+            {
+                type: 'number',
+                name: 'num2',
+                message: 'enter number 2',
+                validate: (answer) => {
+                    if (isNaN(answer)) {
+                        return "Please enter a valid number";
+                    }
+                    return true;
+                }
+            }
+        ]);
+
+    switch (answer.operator) {
+        case "addition":
+            console.log(`${answer.num1} + ${answer.num2} = ${answer.num1 + answer.num2} `);
+            break;
+        case "subtraction":
+            console.log(`${answer.num1} - ${answer.num2} = ${answer.num1 - answer.num2} `);
+            break;
+        case "multiplication":
+            console.log(`${answer.num1} * ${answer.num2} = ${answer.num1 * answer.num2} `);
+            break;
+        case "division":
+            console.log(`${answer.num1} / ${answer.num2} = ${answer.num1 / answer.num2} `);
+            break;
+        default:
+            return 0
+    }
+}
+
+async function askAgain() {
+    do {
+        await askQuestion()
+        var again = await inquirer.prompt({
+            type: "input",
+            name: "restart",
+            message: "Do you want to continue? press y or n: "
+        })
+    } while (again.restart === "y" || again.restart === "Y" || again.restart === "yes" || again.restart === "YES");
+}
+
+askAgain()
