abfbdd2b2ee83865e49c869af76bf528919b414c
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2022-12-05 11:57:15 +0500

    I have created a welcome screen for our cli base calculator.

---
+++ app.ts
@@ -0,0 +1,51 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import gradient from "gradient-string";
+
+let sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    })
+}
+
+async function welcome() {
+   let rainbowTitle =  chalkAnimation.rainbow("let's start calculation...");
+   await sleep()  
+   rainbowTitle.stop(); 
+   console.log(`
+    ${
+      chalk.cyanBright(
+        `
+        _____________________
+        |  _________________  |
+        | | JO           0. | |
+        | |_________________| |
+        |  ___ ___ ___   ___  |
+        | | 7 | 8 | 9 | | + | |
+        | |___|___|___| |___| |
+        | | 4 | 5 | 6 | | - | |
+        | |___|___|___| |___| |
+        | | 1 | 2 | 3 | | x | |
+        | |___|___|___| |___| |
+        | | . | 0 | = | | / | |
+        | |___|___|___| |___| |
+        |_____________________|
+        
+        `
+      )
+    }    
+    `
+   );
+
+   console.log(    
+    chalk.cyanBright(     
+       gradient.pastel.multiline(figlet.textSync("CALCULATOR", {horizontalLayout: 'full'}))         
+    )    
+   );
+
+
+}
+await welcome()
